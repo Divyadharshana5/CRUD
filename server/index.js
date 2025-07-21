a69e15e48dcb59a7c92b2b58e8f6e37004eb4aad
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-21 17:27:48 +0530

    Add console log to indicate server is running on port 3030

--- server/index.js
+++ server/index.js
@@ -2,4 +2,6 @@ const express = require("express");
 const app = express;
 const port = 3030;
 
-app.listen(port, (err) => {});
+app.listen(port, (err) => {
+  console.log(`Api is running on port ${port}`);
+});
