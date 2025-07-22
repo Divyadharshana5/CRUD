7a5e4cd337e278f89ff4d12eb4c67aa8ce5115de
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-22 12:38:54 +0530

    Return user data from /users endpoint in index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const users = require("./sample.json"); // Assuming users.json is in the same directory
 const app = express;
 const port = 3030;
 
 // Display All Users
-app.get("/users", (req, res) => {});
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
 
 app.listen(port, (err) => {
   console.log(`Api is running on port ${port}`);
