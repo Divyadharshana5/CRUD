121276a6ecefaaf7bfeaeffeaee3b75f700a5f86
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-22 11:39:17 +0530

    Add route to display all users in index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express;
 const port = 3030;
 
+// Display All Users
+app.get("/users", (req, res) => {});
+
 app.listen(port, (err) => {
   console.log(`Api is running on port ${port}`);
 });
