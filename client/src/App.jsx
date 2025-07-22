7285c04c1960a54bcc7543f15234411dc02ca48a
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-22 13:02:01 +0530

    Update index.html and App.jsx to enhance the CRUD application UI with a new title and input elements for adding records.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div></div>
+      <div className="container">
+        <h3>CRUD Application with React.js Frontend and Node.js Backend </h3>
+        <div className="input-search">
+          <input type="search" />
+          <button>Add Record</button>
+          <table className="table"></table>
+        </div>
+      </div>
     </>
   );
 }
