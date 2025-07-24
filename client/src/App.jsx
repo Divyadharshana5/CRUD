b9f31571df7c4684b377b0d91e98a22802b046ce
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-24 14:11:28 +0530

    Implement getAllUsers function in App.jsx and update useEffect to call it for future data fetching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,11 @@ import "./App.css";
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {}, []);
+  const getAllUsers = async () => {};
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
   return (
     <div className="container">
       <h3>CRUD Application with React.js Frontend and Node.js Backend</h3>
