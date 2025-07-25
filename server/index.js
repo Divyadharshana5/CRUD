e48057647866c34d7b65d216b22b4e7073f5a90e
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-25 19:31:05 +0530

    Add CORS middleware in index.js to allow requests from React app for improved API accessibility

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const users = require("./sample.json"); // Assuming users.json is in the same directory
+const cors = require("cors");
 
 const app = express();
 const port = 3030;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Adjust this to your React app's URL
+  })
+);
 // Display All Users
 app.get("/users", (req, res) => {
   return res.json(users);
